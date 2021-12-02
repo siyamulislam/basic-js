c824f95fd2bb4c81d900b7ec5b07393d609448cd
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2021-12-02 12:36:44 +0600

    switch added

---
+++ switch.js
@@ -0,0 +1,21 @@
+num=220
+switch(num){
+    case 2: 
+    console.log("i am 2")
+    break
+    case 5: 
+    console.log("i am 5")
+    break
+    case 22: 
+    console.log("i am 22")
+    break
+    case 220: 
+    console.log("i am 220")
+    break
+    case 100: 
+    console.log("i am 100")
+    break
+    default: 
+    console.log("i dont know who are you!")
+    break
+}
