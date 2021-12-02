5a75b467e1d825b2e2cfe597336a128df6aa8ae8
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2021-12-02 12:35:26 +0600

    function added

---
+++ function.js
@@ -0,0 +1,21 @@
+function baby() {
+    console.log("oye baby!")
+    console.log("koi go")
+    console.log("eti aso na")
+    console.log("jaw brush kore aso! lol")
+}
+function magic(num) {
+    var sum = num * 2
+    return sum
+}
+function mul(num1,num2){
+    return num1*num2
+}
+baby()
+
+var first =magic(20)
+var second =magic(100)
+console.log(first,second)
+console.log(first+second)
+var mr=mul(40,2)
+console.log("the Multiply is: "+mr)
