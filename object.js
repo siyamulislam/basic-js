3aac164384b750f27417d74ddb0cd9bc02a1b66f
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2021-12-02 12:37:51 +0600

    Object create, view & update

---
+++ object.js
@@ -0,0 +1,11 @@
+var student ={id:122,phone:54333,namee:"kalam"}
+var student2={id:242,phone:4543,namee:"soniya"}
+
+console.log(student2)
+console.log(student.phone)
+var sName="namee"
+console.log(student2[sName]) 
+student2.namee="mahi"
+student2.age=43
+console.log(student2["namee"]) 
+console.log(student2)
